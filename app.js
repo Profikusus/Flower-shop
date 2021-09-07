ef45ff4eba937a40317356b30c1def47f8e117c8
Profikusus <profikusus@gmail.com>
2021-09-07 14:36:05 +0300

    add

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let navMenu = document.querySelector(".nav-menu");
-let mainMenu = document.querySelector(".menu");
+// let navMenu = document.querySelector(".nav-menu");
+// let mainMenu = document.querySelector(".menu");
 
-navMenu.addEventListener("click", function () {
-  navMenu.classList.toggle("active-menu");
-  if (navMenu.classList.contains("active-menu")) {
-    mainMenu.classList.add("active-menu");
-  } else {
-    mainMenu.classList.remove("active-menu");
-  }
-});
+// navMenu.addEventListener("click", function () {
+//   navMenu.classList.toggle("active-menu");
+//   if (navMenu.classList.contains("active-menu")) {
+//     mainMenu.classList.add("active-menu");
+//   } else {
+//     mainMenu.classList.remove("active-menu");
+//   }
+// });
 
-$(".parallax-window").parallax({ imageSrc: "./images/IMG_4241_.jpg" });
+$(".slider-block").slick();
