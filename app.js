64f941a5f675de75aeb7023ce6e85003609c2e5e
Profikusus <profikusus@gmail.com>
2021-09-07 10:11:08 +0300

    added paralax scroll and tree main block

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let navMenu = document.querySelector(".nav-menu");
 let mainMenu = document.querySelector(".menu");
 
-navMenu.addEventListener("click", function() {
-    navMenu.classList.toggle("active-menu");
-    if(navMenu.classList.contains("active-menu")) {
-        mainMenu.classList.add("active-menu");
-    } else {
-        mainMenu.classList.remove("active-menu")
-    }
-    
+navMenu.addEventListener("click", function () {
+  navMenu.classList.toggle("active-menu");
+  if (navMenu.classList.contains("active-menu")) {
+    mainMenu.classList.add("active-menu");
+  } else {
+    mainMenu.classList.remove("active-menu");
+  }
 });
+
+$(".parallax-window").parallax({ imageSrc: "./images/IMG_4241_.jpg" });
